9c7fba2c14598fbfcaa414d86226402d59853621
Milind Randive <randivemilind1@gmail.com>
2025-02-21 19:16:11 +0530

    pushed to git hub

---
+++ src/Pages/Sell.jsx
@@ -0,0 +1,15 @@
+import StartNavbar from '../components/StartNavbar'
+import MiddleNavbar from '../components/MiddleNavbar'
+
+
+function Sell() {
+  return (
+    <div>
+      <StartNavbar/>
+      <MiddleNavbar/>
+    </div>
+
+  )
+}
+
+export default Sell
